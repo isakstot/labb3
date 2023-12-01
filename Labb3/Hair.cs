7a70c7381747d1eba70529a43502df8b6c23e52a
isakstot <h23issto@du.se>
2023-12-01 09:32:14 +0100

    first commit

---
+++ Labb3/Hair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb3
+{
+    public struct Hair
+    {
+        public string Color;
+        public int LenghtCM;
+    }
+}
---
+++ Labb3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Labb3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gender gender = Gender.Male;
+            Hair hairCM = new Hair { Color = "Brown", LenghtCM = 15 };
+            DateTime birthday = new DateTime(2004, 06, 28);
+            string eyeColor = "Green";
+
+            Person examplePerson = new Person(gender, hairCM, birthday, eyeColor);
+            Console.WriteLine(examplePerson.ToString());
+        }
+    }
+}
