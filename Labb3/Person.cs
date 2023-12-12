4ea0c44b76b7dffeca49c76f5f9fa4540e149699
isakstot <h23issto@du.se>
2023-12-12 14:53:27 +0100

    Fixed formatting in ToString() method for the Person class

--- Labb3/Person.cs
+++ Labb3/Person.cs
@@ -13,7 +13,6 @@ namespace Labb3
         public DateTime Birthday { get; private set; }
         public string EyeColor { get; private set; }
 
-
         public Person (Gender gender, Hair hair, DateTime birthday, string eyeColor)
         {
             TheGender = gender;
@@ -27,10 +26,9 @@ namespace Labb3
             return
                 $"Gender: {TheGender}\n" +
                 $"Hair color: {TheHair.Color}\n" +
-                $"Hair lenght {TheHair.LenghtCM} (cm)\n" +
+                $"Hair lenght: {TheHair.LenghtCM} (cm)\n" +
                 $"Date of birth: {Birthday:yyyy:MM:dd} (yyyy:MM:dd)\n" +
                 $"Eye color: {EyeColor}";
         }
-
     }
 }
